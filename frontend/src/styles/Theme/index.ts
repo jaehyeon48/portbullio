8cf5c21f12cfac9f4379f60a33622b82b457e2e0
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-21 14:57:32 +0900

    feat: DefaultTheme 타입에 현재 theme mode를 나타내는 currentTheme 속성 추가
    추후 styled-components 라이브러리의 useTheme을 통해 사용할 수 있도록 하기 위해 currentTheme: Theme 속성 추가

--- frontend/src/styles/Theme/index.ts
+++ frontend/src/styles/Theme/index.ts
@@ -3,6 +3,7 @@ import { DefaultTheme } from 'styled-components';
 const primary = '#12B886';
 
 export const lightTheme: DefaultTheme = {
+	currentTheme: 'light',
 	base: {
 		bgColor: '#F8F9FA',
 		textColor: '#000',
@@ -11,6 +12,7 @@ export const lightTheme: DefaultTheme = {
 };
 
 export const darkTheme: DefaultTheme = {
+	currentTheme: 'dark',
 	base: {
 		bgColor: '#1A1C34',
 		textColor: '#FFF',
--- frontend/src/types/styled.d.ts
+++ frontend/src/types/styled.d.ts
@@ -1,7 +1,9 @@
 import 'styled-components';
+import { Theme } from '@types';
 
 declare module 'styled-components' {
 	export interface DefaultTheme {
+		currentTheme: Theme;
 		base: {
 			bgColor: string;
 			textColor: string;
