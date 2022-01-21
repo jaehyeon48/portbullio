91a1be8094766ba564016d53d9b025c9945103b3
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-21 18:03:59 +0900

    feat: flexMixin을 별도의 파일로 분리하고 타입 수정
    flexDirection 옵션 추가 및 각 옵션별 타입을 세밀하게 지정

--- frontend/src/styles/mixins/index.ts
+++ frontend/src/styles/mixins/index.ts
@@ -1,12 +1,1 @@
-import { css } from 'styled-components';
-
-interface Flex {
-	alignItems?: string;
-	justifyContent?: string;
-}
-
-export const flexMixin = css<Flex>`
-	display: flex;
-	align-items: ${({ alignItems }) => alignItems ?? 'normal'};
-	justify-content: ${({ justifyContent }) => justifyContent ?? 'normal'};
-`;
+export { default as flexMixin } from './flexMixin';
