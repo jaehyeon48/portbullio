e1585cce3098e51fe7b2c9c6381b92f0efd15211
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-24 20:26:24 +0900

    style: 'Card' styled-component에 color 속성 추가

--- frontend/src/components/Card/style/index.ts
+++ frontend/src/components/Card/style/index.ts
@@ -8,6 +8,7 @@ interface CardStyleProps {
 export default styled.div<CardStyleProps>`
 	position: relative;
 	background-color: ${({ theme }) => theme.card.bgColor};
+	color: ${({ theme }) => theme.base.textColor};
 	border-radius: 8px;
 	box-shadow: ${({ theme }) => theme.card.boxShadow};
 	width: ${({ _width }) => _width ?? '100%'};
