7359de8e32b2266c590a7bc27e49d5feb063d8f6
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-27 19:19:19 +0900

    feat: react-testing-library CustomWrapper 수정
    Wrapper에 BrowserRouter 컴포넌트 추가

--- frontend/src/api/testingLibrary/index.tsx
+++ frontend/src/api/testingLibrary/index.tsx
@@ -1,9 +1,14 @@
 import { ReactElement } from 'react';
 import { ThemeProvider, DefaultTheme } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 
 function CustomWrapper({ children, theme }: { children: ReactElement; theme: DefaultTheme }) {
-	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</BrowserRouter>
+	);
 }
 
 export * from '@testing-library/react';
