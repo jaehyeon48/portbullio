d8fba7efe993360ef0d811261c2da872aa42a150
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-27 11:49:46 +0900

    feat: morgan 라이브러리 설치

--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import { LoaderProps } from '@types';
 import api from '@api';
+import { morgan, formatMorgan } from './morgan';
 
 export default async function expressLoader({ app }: LoaderProps) {
+	app.use(morgan(formatMorgan));
 	app.use(express.json());
 	app.use(cookieParser());
 	app.use('/api', api());
