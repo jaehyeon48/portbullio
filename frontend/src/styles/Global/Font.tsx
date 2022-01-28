5b12956df33a1e51bf4e214844e732a79bf6412c
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-28 22:36:52 +0900

    style: 폰트를 나눔 고딕에서 noto sans kr로 변경

--- frontend/src/styles/Global/Font.tsx
+++ frontend/src/styles/Global/Font.tsx
@@ -1,24 +1,36 @@
 import { createGlobalStyle } from 'styled-components';
-import NanumGothicRegularWoff2 from '@assets/fonts/NanumGothic.woff2';
-import NanumGothicRegularWoff from '@assets/fonts/NanumGothic.woff';
-import NanumGothicBoldWoff2 from '@assets/fonts/NanumGothicBold.woff2';
-import NanumGothicBoldWoff from '@assets/fonts/NanumGothicBold.woff';
+import notoSansKR400Woff2 from '@assets/fonts/noto_sans_kr_400.woff2';
+import notoSansKR400Woff from '@assets/fonts/noto_sans_kr_400.woff';
+import notoSansKR500Woff2 from '@assets/fonts/noto_sans_kr_500.woff2';
+import notoSansKR500Woff from '@assets/fonts/noto_sans_kr_500.woff';
+import notoSansKR700Woff2 from '@assets/fonts/noto_sans_kr_700.woff2';
+import notoSansKR700Woff from '@assets/fonts/noto_sans_kr_700.woff';
 
 export default createGlobalStyle`
   @font-face {
-  	font-family: 'NanumGothic';
+  	font-family: 'NotoSansKR';
   	font-style: normal;
   	font-weight: 400;
-  	src: local('NanumGothic'),
-      url(${NanumGothicRegularWoff2}) format('woff2'),
-  	  url(${NanumGothicRegularWoff}) format('woff');
+  	src: local('NotoSansKR'),
+      url(${notoSansKR400Woff2}) format('woff2'),
+  	  url(${notoSansKR400Woff}) format('woff');
   }
+
+	@font-face {
+  	font-family: 'NotoSansKR';
+  	font-style: normal;
+  	font-weight: 500;
+  	src: local('NotoSansKR'),
+      url(${notoSansKR500Woff2}) format('woff2'),
+  	  url(${notoSansKR500Woff}) format('woff');
+  }
+
   @font-face {
-  	font-family: 'NanumGothic';
+  	font-family: 'NotoSansKR';
   	font-style: normal;
   	font-weight: 700;
-  	src: local('NanumGothic'),
-      url(${NanumGothicBoldWoff2}) format('woff2'),
-  	  url(${NanumGothicBoldWoff}) format('woff');
+  	src: local('NotoSansKR'),
+      url(${notoSansKR700Woff2}) format('woff2'),
+  	  url(${notoSansKR700Woff}) format('woff');
   }
 `;
--- frontend/src/styles/Global/Style.tsx
+++ frontend/src/styles/Global/Style.tsx
@@ -3,7 +3,7 @@ import { flexMixin } from '../mixins';
 
 export default createGlobalStyle`
   body {
-    font-family: NanumGothic, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: NotoSansKR, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     background-color: ${({ theme }) => theme.base.bgColor};
     color: ${({ theme }) => theme.base.textColor};
   }
