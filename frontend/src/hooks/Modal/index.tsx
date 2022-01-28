3b7c223c6920b251e6c629ac2c1f335987dfa8f5
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-28 20:44:42 +0900

    feat: Modal에서 사용된 createPortal 제거
    React Portal대신 useModal과 연계하여 ReactDOM.render를 사용하도록 구조 수정

--- frontend/src/hooks/Modal/index.tsx
+++ frontend/src/hooks/Modal/index.tsx
@@ -1,25 +1,41 @@
-import { useState, SyntheticEvent } from 'react';
+import { useState, SyntheticEvent, ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+import AppProviders from '@components/AppProviders';
+import Modal from '@components/Modal';
+import useThemeMode from '@hooks/Theme';
+import { lightTheme, darkTheme } from '@styles/Theme';
 
+type OpenModalFn = (e: SyntheticEvent, children: ReactNode) => void;
 type SetterFn = (e: SyntheticEvent) => void;
 
 interface UseModalReturnType {
 	isOpen: boolean;
-	openModal: SetterFn;
+	openModal: OpenModalFn;
 	closeModal: SetterFn;
 	toggleModal: SetterFn;
 }
 
 export function useModal(): UseModalReturnType {
+	const modalRootElem = document.getElementById('modal-root') as Element;
 	const [isOpen, setIsOpen] = useState(false);
-
-	const openModal = (e: SyntheticEvent) => {
-		e.stopPropagation();
-		setIsOpen(true);
-	};
+	const [themeMode] = useThemeMode();
+	const theme = themeMode === 'light' ? lightTheme : darkTheme;
 
 	const closeModal = (e: SyntheticEvent) => {
 		e.stopPropagation();
+		ReactDOM.unmountComponentAtNode(modalRootElem);
 		setIsOpen(false);
+	};
+
+	const openModal = (e: SyntheticEvent, children: ReactNode) => {
+		e.stopPropagation();
+		ReactDOM.render(
+			<AppProviders theme={theme}>
+				<Modal closeFunction={closeModal}>{children}</Modal>
+			</AppProviders>,
+			modalRootElem
+		);
+		setIsOpen(true);
 	};
 
 	const toggleModal = (e: SyntheticEvent) => {
