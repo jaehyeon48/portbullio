71cee36fec0aa2bae4265b729f39c494bdc0465e
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-31 08:22:58 +0900

    sessionService의 'createSession' 로직 구현

---
+++ backend/src/services/session.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError } from 'axios';
+import { logger } from '@loaders';
+import envConfig from '@config';
+
+interface NewSessionId {
+	newSessionId: string;
+}
+
+interface CreateSessionResponse {
+	data: NewSessionId;
+}
+
+export async function createSession(userId: number): Promise<string> {
+	const { sessionServerURL } = envConfig;
+	try {
+		const { data }: CreateSessionResponse = await axios.post(`${sessionServerURL}/${userId}`);
+
+		return data.newSessionId;
+	} catch (error) {
+		const err = error as AxiosError;
+		logger.error(err.message);
+		logger.error(`${err.response?.status}: ${JSON.stringify(err.response?.data)}`);
+		throw err;
+	}
+}
