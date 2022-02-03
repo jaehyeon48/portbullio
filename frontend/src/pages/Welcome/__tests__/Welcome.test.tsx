4ebebe4db2153e03c100ef6207ae61243886aac9
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-03 16:20:45 +0900

    feat: Welcome 페이지 hero 이미지 테마 별로 구분

--- frontend/src/pages/Welcome/__tests__/Welcome.test.tsx
+++ frontend/src/pages/Welcome/__tests__/Welcome.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen, CustomWrapper } from '@api/testingLibrary';
-import { lightTheme } from '@src/styles/Theme';
+import { lightTheme, darkTheme } from '@src/styles/Theme';
 import Welcome from '../Welcome';
 
-test('Welcome page test', () => {
+test('Welcome page test on light theme', () => {
 	render(
 		<CustomWrapper theme={lightTheme}>
 			<Welcome />
@@ -11,8 +11,19 @@ test('Welcome page test', () => {
 
 	const welcomeText = screen.getByText(/회원가입을 축하합니다/);
 	const createNewPortfolioLink = screen.getByRole('link');
-	const heroImage = screen.getByLabelText('Welcome page hero image');
+	const heroImage = screen.getByLabelText('Welcome page hero image light theme');
 	expect(welcomeText).toHaveStyle('color: #12B886');
 	expect(createNewPortfolioLink).toBeInTheDocument();
+	expect(heroImage).toBeInTheDocument();
+});
+
+test('Welcome page test on dark theme', () => {
+	render(
+		<CustomWrapper theme={darkTheme}>
+			<Welcome />
+		</CustomWrapper>
+	);
+
+	const heroImage = screen.getByLabelText('Welcome page hero image dark theme');
 	expect(heroImage).toBeInTheDocument();
 });
