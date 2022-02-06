b4037f4950ff58d7662e4f6532ca17ff4d32aef6
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-06 20:30:18 +0900

    refactor: sessionValidator 형식을 함수 표현식에서 함수 선언식으로 수정

--- backend/src/middlewares/sessionValidator.ts
+++ backend/src/middlewares/sessionValidator.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { sessionService } from '@services';
 
-const sessionValidator = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export default async function sessionValidator(
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> {
 	try {
 		const sessionId = req.cookies.uaat;
 		const userId = await sessionService.checkSession(sessionId);
@@ -12,6 +16,4 @@ const sessionValidator = async (req: Request, res: Response, next: NextFunction)
 	} catch (error) {
 		next(error);
 	}
-};
-
-export default sessionValidator;
+}
