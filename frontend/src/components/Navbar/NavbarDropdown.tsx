6e91ff1f1fec59c3b2a1dc280e62e00db2ef6811
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-11 22:55:55 +0900

    refactor: NavbarDropdown 프로필 설정과 로그 아웃 위치 스왑

--- frontend/src/components/Navbar/NavbarDropdown.tsx
+++ frontend/src/components/Navbar/NavbarDropdown.tsx
@@ -8,11 +8,11 @@ interface Props {
 export default function NavbarDropdown({ logOutFn, profileFn }: Props) {
 	return (
 		<Style.DropdownContainer>
-			<Style.DropdownButton type="button" onClick={logOutFn}>
-				로그아웃
-			</Style.DropdownButton>
 			<Style.DropdownButton type="button" onClick={profileFn}>
 				프로필 설정
+			</Style.DropdownButton>
+			<Style.DropdownButton type="button" onClick={logOutFn}>
+				로그아웃
 			</Style.DropdownButton>
 		</Style.DropdownContainer>
 	);
