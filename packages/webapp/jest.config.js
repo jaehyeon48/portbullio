ae216dfbb9afdf35fe5895e8fa38ba814bbc7691
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-15 21:12:05 +0900

    webapp - config: ts-jest 설치 및 설정

--- packages/webapp/jest.config.js
+++ packages/webapp/jest.config.js
@@ -1,4 +1,9 @@
 export default {
+	preset: "ts-jest",
+	transform: {
+		'^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest"
+	},
 	setupFilesAfterEnv: ['./src/setupTests.ts'],
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
