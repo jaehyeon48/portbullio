8889806d37a241ceb01322380aaaf14353e75a65
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-18 16:24:17 +0900

    webapp - fix: calculatePriceColor 수정
    1. value === 0 일 때 'var(--gray)' 색상을 적용하도록 수정

--- packages/webapp/src/styles/Mixins/priceColorMixin.ts
+++ packages/webapp/src/styles/Mixins/priceColorMixin.ts
@@ -1,20 +1,20 @@
-import { css, DefaultTheme } from 'styled-components';
+import { css } from 'styled-components';
 
 export interface PriceColorProps {
 	value: number;
 }
 
-function calculatePriceColor(theme: DefaultTheme, value: number) {
+function calculatePriceColor(value: number) {
 	if (value > 0) return 'var(--priceGreen)';
 	if (value < 0) return 'var(--priceRed)';
-	return theme.base.colors.gray;
+	return 'var(--gray)';
 }
 
 const priceColorMixin = css<PriceColorProps>`
-	color: ${({ theme, value }) => calculatePriceColor(theme, value)};
+	color: ${({ value }) => calculatePriceColor(value)};
 
 	& svg {
-		fill: ${({ theme, value }) => calculatePriceColor(theme, value)};
+		fill: ${({ value }) => calculatePriceColor(value)};
 	}
 `;
 
