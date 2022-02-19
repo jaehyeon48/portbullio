f5547c338f9a3f7845ceb93f1cc996a5090cf87a
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-20 00:16:34 +0900

    webapp - feat: 아이콘 추가 #29
    1. 동그라미 체크 아이콘 추가
    2. 필터 아이콘 추가
    3. 자물쇠 아이콘 추가
    4. 연필 아이콘 추가
    5. 플러스(+) 아이콘 추가
    6. 돋보기 아이콘 추가
    7. 쓰레기통 아이콘 추가

---
+++ packages/webapp/src/components/Icon/CircleCheck.tsx
@@ -0,0 +1,13 @@
+import { SVG } from '@components/index';
+import { IconProps } from '@types';
+
+export default function CircleCheck({ width = 24, height = 24, fill = 'primary' }: IconProps) {
+	return (
+		<SVG width={width} height={height} viewBox="0 0 24 24" fill={fill} aria-label="icon">
+			<g fill="none" stroke="currentColor" strokeWidth="2">
+				<path strokeLinecap="round" strokeLinejoin="round" d="m8 12.5l3 3l5-6" />
+				<circle cx="12" cy="12" r="10" />
+			</g>
+		</SVG>
+	);
+}
---
+++ packages/webapp/src/components/Icon/Plus.tsx
@@ -0,0 +1,14 @@
+import { SVG } from '@components/index';
+import { IconProps } from '@types';
+
+export default function Plus({ width = 32, height = 32, fill = 'primary' }: IconProps) {
+	return (
+		<SVG width={width} height={height} viewBox="0 0 1024 1024" fill={fill} aria-label="icon">
+			<path
+				d="M512 512H170.667M512 853.333V512V853.333ZM512 512V170.667V512ZM512 512H853.333H512Z"
+				strokeWidth="90"
+				strokeLinecap="round"
+			/>
+		</SVG>
+	);
+}
