a639fe579972f1227324dee76cdc7515cbf133c2
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-26 12:37:13 +0900

    webapp - feat: 기존의 Stock Main 페이지 테스트에 TC 추가
    주식 가격, 주식 가격 변동 퍼센테이지, 주식 메뉴 링크 테스트 케이스 추가

--- packages/webapp/src/pages/Stock/Main/__tests__/StockMain.test.tsx
+++ packages/webapp/src/pages/Stock/Main/__tests__/StockMain.test.tsx
@@ -2,26 +2,67 @@ import { render, screen } from '@api/testingLibrary';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import StockMain from '../StockMain';
 
-test('Should render AAPL stock page', () => {
-	render(
-		<MemoryRouter initialEntries={['/stock/aapl']}>
-			<Routes>
-				<Route path="stock/:ticker" element={<StockMain />} />
-			</Routes>
-		</MemoryRouter>
-	);
+describe('Stock Main Layout', () => {
+	test('Should render AAPL stock page', () => {
+		render(
+			<MemoryRouter initialEntries={['/stock/aapl']}>
+				<Routes>
+					<Route path="stock/:ticker" element={<StockMain />} />
+				</Routes>
+			</MemoryRouter>
+		);
 
-	expect(screen.getByLabelText('Ticker')).toHaveTextContent('AAPL');
-});
+		expect(screen.getByLabelText('Ticker')).toHaveTextContent('AAPL');
+	});
 
-test('Should render MSFT stock page', () => {
-	render(
-		<MemoryRouter initialEntries={['/stock/msft']}>
-			<Routes>
-				<Route path="stock/:ticker" element={<StockMain />} />
-			</Routes>
-		</MemoryRouter>
-	);
+	test('Should render MSFT stock page', () => {
+		render(
+			<MemoryRouter initialEntries={['/stock/msft']}>
+				<Routes>
+					<Route path="stock/:ticker" element={<StockMain />} />
+				</Routes>
+			</MemoryRouter>
+		);
 
-	expect(screen.getByLabelText('Ticker')).toHaveTextContent('MSFT');
+		expect(screen.getByLabelText('Ticker')).toHaveTextContent('MSFT');
+	});
+
+	test('Should have a current price component', () => {
+		render(
+			<MemoryRouter initialEntries={['/stock/aapl']}>
+				<Routes>
+					<Route path="stock/:ticker" element={<StockMain />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		expect(screen.getByLabelText('Current price')).toBeInTheDocument();
+	});
+
+	test('Should have a price change component', () => {
+		render(
+			<MemoryRouter initialEntries={['/stock/aapl']}>
+				<Routes>
+					<Route path="stock/:ticker" element={<StockMain />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		expect(screen.getByLabelText('Price change')).toBeInTheDocument();
+	});
+
+	test('Should have four stock menu links', () => {
+		render(
+			<MemoryRouter initialEntries={['/stock/aapl']}>
+				<Routes>
+					<Route path="stock/:ticker" element={<StockMain />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		expect(screen.getByRole('link', { name: '개요' })).toBeInTheDocument();
+		expect(screen.getByRole('link', { name: '기업 정보' })).toBeInTheDocument();
+		expect(screen.getByRole('link', { name: '재무 정보' })).toBeInTheDocument();
+		expect(screen.getByRole('link', { name: '뉴스' })).toBeInTheDocument();
+	});
 });
