13d0757f199e5ec86faadbb8ddf2ff35f34c3779
jaehyeon48 <imjaehyeon48@gmail.com>
2022-02-27 15:00:47 +0900

    webapp - fix: CircleCheck 아이콘 strokeWidth 3으로 수정

--- packages/webapp/src/components/Icon/CircleCheck.tsx
+++ packages/webapp/src/components/Icon/CircleCheck.tsx
@@ -4,7 +4,7 @@ import { IconProps } from '@types';
 export default function CircleCheck({ width = 24, height = 24, fill = 'primary' }: IconProps) {
 	return (
 		<SVG width={width} height={height} viewBox="0 0 24 24" fill={fill} aria-label="icon">
-			<g fill="none" stroke="currentColor" strokeWidth="2">
+			<g fill="none" stroke="currentColor" strokeWidth="3">
 				<path strokeLinecap="round" strokeLinejoin="round" d="m8 12.5l3 3l5-6" />
 				<circle cx="12" cy="12" r="10" />
 			</g>
