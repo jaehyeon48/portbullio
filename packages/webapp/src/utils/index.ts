50dd5bb6994408199fd292a9351430f69b5d8082
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-06 21:10:02 +0900

    webapp - feat: isValidRealNumber 유틸함수 구현
    올바른 실수 문자열인지를 판별하는 함수 구현

--- packages/webapp/src/utils/index.ts
+++ packages/webapp/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as formatNum } from './formatNumber';
 export { default as formatCurrency } from './formatCurrency';
 export { default as asyncThrottleAndDebounce } from './asyncThrottleAndDebounce';
+export { default as isValidRealNumber } from './isValidRealNumber';
