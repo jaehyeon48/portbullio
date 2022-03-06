331661bfb7b1596f681e3b54f302462d4a421db9
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-06 16:45:08 +0900

    webapp - refactor: Stock/styles.ts 파일의 baseFontSize 상수 객체 이름을 BASE_FONT_SIZE_PX로 수정

--- packages/webapp/src/pages/Stock/styles.ts
+++ packages/webapp/src/pages/Stock/styles.ts
@@ -1,14 +1,15 @@
 import { css } from 'styled-components';
 import { WIDTH_BREAK_POINT_PX } from '@constants/breakPoints';
 
-export const baseFontSize = {
+export const BASE_FONT_SIZE_PX = {
 	desktop: 16,
 	laptop: 14
 };
+
 export const stockPageBaseFontStyle = css`
-	font-size: ${baseFontSize.desktop}px;
+	font-size: ${BASE_FONT_SIZE_PX.desktop}px;
 
 	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptop}px) {
-		font-size: ${baseFontSize.laptop}px;
+		font-size: ${BASE_FONT_SIZE_PX.laptop}px;
 	}
 `;
