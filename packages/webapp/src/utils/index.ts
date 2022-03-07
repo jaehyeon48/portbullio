fb928ce180c62e86bc85e8c7a999667246acd191
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-07 13:55:49 +0900

    webapp - feat: isValidInteger 유틸함수 구현
    올바른 정수 문자열인지를 판별하는 함수 구현

--- packages/webapp/src/utils/index.ts
+++ packages/webapp/src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as formatNum } from './formatNumber';
 export { default as formatCurrency } from './formatCurrency';
 export { default as asyncThrottleAndDebounce } from './asyncThrottleAndDebounce';
 export { default as isValidRealNumber } from './isValidRealNumber';
+export { default as isValidInteger } from './isValidInteger';
