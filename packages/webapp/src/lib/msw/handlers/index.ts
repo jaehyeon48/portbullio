cb7857e08ce1dc5eba10b723666db25b01434a41
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-10 13:55:55 +0900

    webapp - feat: getAvatar msw 핸들러 구현

--- packages/webapp/src/lib/msw/handlers/index.ts
+++ packages/webapp/src/lib/msw/handlers/index.ts
@@ -1,10 +1,12 @@
 import { rest } from 'msw';
 import envConfig from '@configs/env';
 import * as portfolioMock from './portfolio';
+import * as userMock from './user';
 
 const handlers = [
 	rest.get(`${envConfig.serverEndPoint}/portfolios`, portfolioMock.getPortfolioList),
-	rest.get(`${envConfig.serverEndPoint}/portfolios/default`, portfolioMock.getDefaultPortfolio)
+	rest.get(`${envConfig.serverEndPoint}/portfolios/default`, portfolioMock.getDefaultPortfolio),
+	rest.get(`${envConfig.serverEndPoint}/user/avatar`, userMock.getAvatar)
 ];
 
 export default handlers;
