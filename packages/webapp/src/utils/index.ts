b05dcaf4c3238d5ba4891492239a4954ce6ac8d9
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-10 18:19:55 +0900

    webapp - feat: formatDate 유틸 함수 구현

--- packages/webapp/src/utils/index.ts
+++ packages/webapp/src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as formatNum } from './formatNumber';
 export { default as formatCurrency } from './formatCurrency';
+export { default as formatDate } from './formatDate';
 export { default as asyncThrottleAndDebounce } from './asyncThrottleAndDebounce';
 export { default as isValidRealNumber } from './isValidRealNumber';
 export { default as isValidInteger } from './isValidInteger';
