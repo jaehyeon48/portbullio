daeeb89c784e20c4d17b3f3c3e8e13b132d08e44
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-15 15:13:43 +0900

    webapp - feat: 실현 손익 퍼센티지가 0보다 큰 경우 앞에 '+' 추가

--- packages/webapp/src/pages/StockTransactions/Main/StockTransactionList.tsx
+++ packages/webapp/src/pages/StockTransactions/Main/StockTransactionList.tsx
@@ -1,7 +1,7 @@
 import { SyntheticEvent } from 'react';
 import { Pencil as PencilIcon, TrashCan as TrashCanIcon } from '@components/Icon';
 import { StockTransactionLog, StockTransactionType } from '@prisma/client';
-import { formatDate, formatCurrency, formatNum } from '@utils';
+import { formatDate, formatCurrency, formatNum, truncateDecimalPoint } from '@utils';
 import { useModal } from '@hooks/Modal';
 import {
 	ListItems,
@@ -112,75 +112,81 @@ export default function StockTransactionList({ stockTransactionList, isLoading }
 				emptyListNoticeMessage="거래 내역이 없습니다."
 			>
 				{stockTransactionList?.map(
-					({ id, createdAt, ticker, transactionType, price, quantity, memo, avgBuyCost }) => (
-						<ListItem key={id}>
-							<Style.DateSection>{formatDate(createdAt as unknown as string)}</Style.DateSection>
-							<Style.TransactionTypeSection>
-								{stockTransactionTypeKor[transactionType as keyof typeof StockTransactionType]}
-							</Style.TransactionTypeSection>
-							<Style.PriceSection>{formatCurrency(price, 'usd')}</Style.PriceSection>
-							<Style.QuantitySection>{formatNum(quantity)}</Style.QuantitySection>
-							<Style.MemoSection>
-								<Style.MemoOpenButton
-									type="button"
-									onClick={e => handleOpenMemoModal({ e, stockTransactionId: id, ticker, memo })}
-								>
-									<StickyNoteIcon />
-								</Style.MemoOpenButton>
-							</Style.MemoSection>
-							<Style.RealizedProfitAndLossSection value={avgBuyCost ? price - avgBuyCost : 0}>
-								<DynamicCaret
-									value={avgBuyCost ? price - avgBuyCost : 0}
-									width={20}
-									height={20}
-									marginTop={0}
-								/>
-								{avgBuyCost &&
-									formatCurrency(
-										truncateDecimalPoint((price - avgBuyCost) * quantity, DECIMAL_DIGITS),
-										'usd'
-									)}
-								{avgBuyCost &&
-									` (${formatNum(
-										truncateDecimalPoint(((price - avgBuyCost) / avgBuyCost) * 100, DECIMAL_DIGITS)
-									)}%)`}
-							</Style.RealizedProfitAndLossSection>
-							<Style.StockTransactionActionsSection>
-								<Style.StockTransactionEditButton
-									type="button"
-									onClick={e =>
-										handleOpenEditTransactionModal({
-											e,
-											stockTransactionId: id,
-											ticker,
-											price,
-											quantity,
-											type: transactionType,
-											date: createdAt as unknown as string
-										})
-									}
-								>
-									<PencilIcon width={16} height={16} />
-									내역 수정
-								</Style.StockTransactionEditButton>
-								<Style.StockTransactionDeleteButton
-									type="button"
-									onClick={e =>
-										handleOpenDeleteConfirmModal({
-											e,
-											stockTransactionId: id,
-											ticker,
-											quantityToDelete: quantity,
-											type: transactionType
-										})
-									}
-								>
-									<TrashCanIcon width={16} height={16} />
-									삭제
-								</Style.StockTransactionDeleteButton>
-							</Style.StockTransactionActionsSection>
-						</ListItem>
-					)
+					({ id, createdAt, ticker, transactionType, price, quantity, memo, avgBuyCost }) => {
+						const realizedProfitLossPercent = avgBuyCost
+							? ((price - avgBuyCost) / avgBuyCost) * 100
+							: 0;
+
+						return (
+							<ListItem key={id}>
+								<Style.DateSection>{formatDate(createdAt as unknown as string)}</Style.DateSection>
+								<Style.TransactionTypeSection>
+									{stockTransactionTypeKor[transactionType as keyof typeof StockTransactionType]}
+								</Style.TransactionTypeSection>
+								<Style.PriceSection>{formatCurrency(price, 'usd')}</Style.PriceSection>
+								<Style.QuantitySection>{formatNum(quantity)}</Style.QuantitySection>
+								<Style.MemoSection>
+									<Style.MemoOpenButton
+										type="button"
+										onClick={e => handleOpenMemoModal({ e, stockTransactionId: id, ticker, memo })}
+									>
+										<StickyNoteIcon />
+									</Style.MemoOpenButton>
+								</Style.MemoSection>
+								<Style.RealizedProfitAndLossSection value={avgBuyCost ? price - avgBuyCost : 0}>
+									<DynamicCaret
+										value={avgBuyCost ? price - avgBuyCost : 0}
+										width={20}
+										height={20}
+										marginTop={0}
+									/>
+									{avgBuyCost &&
+										formatCurrency(
+											truncateDecimalPoint((price - avgBuyCost) * quantity, DECIMAL_DIGITS),
+											'usd'
+										)}
+									{avgBuyCost &&
+										` (${realizedProfitLossPercent > 0 ? '+' : ''}${formatNum(
+											truncateDecimalPoint(realizedProfitLossPercent, DECIMAL_DIGITS)
+										)}%)`}
+								</Style.RealizedProfitAndLossSection>
+								<Style.StockTransactionActionsSection>
+									<Style.StockTransactionEditButton
+										type="button"
+										onClick={e =>
+											handleOpenEditTransactionModal({
+												e,
+												stockTransactionId: id,
+												ticker,
+												price,
+												quantity,
+												type: transactionType,
+												date: createdAt as unknown as string
+											})
+										}
+									>
+										<PencilIcon width={16} height={16} />
+										내역 수정
+									</Style.StockTransactionEditButton>
+									<Style.StockTransactionDeleteButton
+										type="button"
+										onClick={e =>
+											handleOpenDeleteConfirmModal({
+												e,
+												stockTransactionId: id,
+												ticker,
+												quantityToDelete: quantity,
+												type: transactionType
+											})
+										}
+									>
+										<TrashCanIcon width={16} height={16} />
+										삭제
+									</Style.StockTransactionDeleteButton>
+								</Style.StockTransactionActionsSection>
+							</ListItem>
+						);
+					}
 				)}
 			</ListItems>
 		</>
@@ -191,7 +197,3 @@ const stockTransactionTypeKor = {
 	buy: '매수',
 	sell: '매도'
 } as const;
-
-function truncateDecimalPoint(number: number, fractionDigits: number) {
-	return Number(number.toFixed(fractionDigits));
-}
