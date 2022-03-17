72cda9773a15c5e46b0d97f47e5c65394505aa98
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-17 12:30:39 +0900

    server - fix: 현금 거래내역 리스트 get, 현금 거래내역 post route에 sessionValidator 적용

--- packages/server/src/api/portfolio/cash.ts
+++ packages/server/src/api/portfolio/cash.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import * as cashService from '@services/cash';
+import { sessionValidator } from '@middlewares';
 import { CashTransactionType } from '@prisma/client';
 
 interface PortfolioIdParam {
@@ -16,34 +17,42 @@ interface AddCashTransactionReqBody {
 export default (): express.Router => {
 	const router = express.Router();
 
-	router.get('/:portfolioId/cash', async (req: Request, res: Response, next: NextFunction) => {
-		const { portfolioId } = req.params as unknown as PortfolioIdParam;
+	router.get(
+		'/:portfolioId/cash',
+		sessionValidator,
+		async (req: Request, res: Response, next: NextFunction) => {
+			const { portfolioId } = req.params as unknown as PortfolioIdParam;
 
-		try {
-			const cashTransactions = await cashService.getCashTransactions(Number(portfolioId));
-			res.json({ cashTransactions });
-		} catch (error) {
-			next(error);
+			try {
+				const cashTransactions = await cashService.getCashTransactions(Number(portfolioId));
+				res.json({ cashTransactions });
+			} catch (error) {
+				next(error);
+			}
 		}
-	});
+	);
 
-	router.post('/:portfolioId/cash', async (req: Request, res: Response, next: NextFunction) => {
-		const { portfolioId } = req.params as unknown as PortfolioIdParam;
-		const { amount, note, type, date } = req.body as unknown as AddCashTransactionReqBody;
+	router.post(
+		'/:portfolioId/cash',
+		sessionValidator,
+		async (req: Request, res: Response, next: NextFunction) => {
+			const { portfolioId } = req.params as unknown as PortfolioIdParam;
+			const { amount, note, type, date } = req.body as unknown as AddCashTransactionReqBody;
 
-		try {
-			const newCashTransaction = await cashService.addCashTransaction({
-				portfolioId: Number(portfolioId),
-				amount,
-				note,
-				type,
-				date
-			});
-			res.json({ newCashTransaction });
-		} catch (error) {
-			next(error);
+			try {
+				const newCashTransaction = await cashService.addCashTransaction({
+					portfolioId: Number(portfolioId),
+					amount,
+					note,
+					type,
+					date
+				});
+				res.json({ newCashTransaction });
+			} catch (error) {
+				next(error);
+			}
 		}
-	});
+	);
 
 	return router;
 };
