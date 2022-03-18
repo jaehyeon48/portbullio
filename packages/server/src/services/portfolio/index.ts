bb9a05faa3c4e38b1856235ea094a9c89dd44ccf
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-18 10:34:04 +0900

    refactor: editPortfolio.ts 파일에 있는 두 개의 함수를 각각의 파일로 분리

--- packages/server/src/services/portfolio/index.ts
+++ packages/server/src/services/portfolio/index.ts
@@ -1,6 +1,7 @@
 export { default as createPortfolio } from './createPortfolio';
 export * from './getPortfolio';
-export * from './editPortfolio';
+export { default as editPortfolioName } from './editPortfolioName';
+export { default as editPortfolioPrivacy } from './editPortfolioPrivacy';
 export { default as deletePortfolio } from './deletePortfolio';
 export { default as setDefaultPortfolio } from './setDefaultPortfolio';
 export { default as getDefaultPortfolio } from './getDefaultPortfolio';
