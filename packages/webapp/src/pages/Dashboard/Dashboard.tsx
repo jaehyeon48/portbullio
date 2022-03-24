68dec2c9ad95205a4576d6a72b90b57c868b93f8
jaehyeon48 <imjaehyeon48@gmail.com>
2022-03-24 11:46:26 +0900

    webapp - feat: 대시보드 페이지에 useTitle 적용

--- packages/webapp/src/pages/Dashboard/Dashboard.tsx
+++ packages/webapp/src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useTitle } from '@hooks/index';
 import { DashboardContainer } from './style';
 import ProfitLossAsset from './ProfitLossAsset';
 import ProportionByValue from './ProportionChart/ProportionByValue';
@@ -6,6 +7,7 @@ import DividendPieChart from './DividendPieChart';
 import AssetHistory from './AssetHistory';
 
 export default function Dashboard() {
+	useTitle(`portbullio - 대시보드`);
 	return (
 		<DashboardContainer>
 			<ProfitLossAsset />
