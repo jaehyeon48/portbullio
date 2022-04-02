ce9a96a537eeab1feb911747f03798502ef087ac
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-02 14:10:03 +0900

    socket - fix: IEX Cloud에 실시간 데이터를 요청할 때 각 티커별로 encodeURIComponent 적용

--- packages/socketServer/src/services/fetchRealtimeData.ts
+++ packages/socketServer/src/services/fetchRealtimeData.ts
@@ -4,7 +4,7 @@ import envConfig from '@config';
 export default async function fetchRealtimeData(tickers: string[][]) {
 	const result = await Promise.all(
 		tickers.map(tickerGroup => {
-			const tickerParam = tickerGroup.join(',');
+			const tickerParam = tickerGroup.map(ticker => encodeURIComponent(ticker)).join(',');
 			return axios.get(
 				`https://cloud.iexapis.com/stable/stock/market/batch?symbols=${tickerParam}&types=quote&token=${envConfig.iexCloudApiKey}`
 			);
