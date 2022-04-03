d595fb31aa81a100b4b8df4ef96a884991e39527
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-03 16:36:33 +0900

    fix: getMarketStatus 로직 수정
    탐색하는key 값을 marketStatus로 수정하고, 만약 해당 키가 없는 경우(null) closed를 기본 값으로 지정

--- packages/server/src/services/stock/getMarketStatus.ts
+++ packages/server/src/services/stock/getMarketStatus.ts
@@ -2,6 +2,6 @@ import { MarketStatus } from '@portbullio/shared/src/types';
 import { marketStatusRedisClient } from '@lib/index';
 
 export default async function getMarketStatus(): Promise<MarketStatus> {
-	const result = await marketStatusRedisClient.get('marketState');
+	const result = (await marketStatusRedisClient.get('marketStatus')) ?? 'closed';
 	return result as MarketStatus;
 }
