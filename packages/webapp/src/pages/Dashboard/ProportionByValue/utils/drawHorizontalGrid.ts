2a763c8dff65ba2c1863501b21af5568030eecc7
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-07 11:33:01 +0900

    webapp - feat: y축 범례 수정
    1. 폰트 크기를 14px로 축소
    2. 글자의 위치를 y축 기준으로 정렬하도록 수정

--- packages/webapp/src/pages/Dashboard/ProportionByValue/utils/drawHorizontalGrid.ts
+++ packages/webapp/src/pages/Dashboard/ProportionByValue/utils/drawHorizontalGrid.ts
@@ -1,5 +1,10 @@
 import { Theme } from '@types';
-import { NUM_OF_HORIZONTAL_GRID, Y_AXIS_MARGIN, HORIZONTAL_GRID_THICKNESS } from '../constants';
+import {
+	NUM_OF_HORIZONTAL_GRID,
+	Y_AXIS_MARGIN,
+	Y_AXIS_LEGEND_GAP,
+	HORIZONTAL_GRID_THICKNESS
+} from '../constants';
 import { textColor, horizontalGridColor } from '../../colors';
 import yPos from './yPos';
 
@@ -19,17 +24,19 @@ export default function drawHorizontalGrid({
 	canvasHeight
 }: Props) {
 	const HORIZONTAL_GRID_GAP = Math.round(maxValue / NUM_OF_HORIZONTAL_GRID);
+	const legendXPos = Y_AXIS_MARGIN - Y_AXIS_LEGEND_GAP;
 	ctx.lineWidth = HORIZONTAL_GRID_THICKNESS;
-	ctx.font = 'bold 16px NotoSansKR';
+	ctx.font = 'bold 14px NotoSansKR';
 	ctx.strokeStyle = horizontalGridColor(theme);
 	ctx.textBaseline = 'middle';
+	ctx.textAlign = 'right';
 	ctx.fillStyle = textColor(theme);
-	ctx.fillText('0%', 0, yPos(0, maxValue, canvasHeight));
+	ctx.fillText('0%', legendXPos, yPos(0, maxValue, canvasHeight));
 	ctx.beginPath();
 	for (let gridVal = HORIZONTAL_GRID_GAP; gridVal < maxValue; gridVal += HORIZONTAL_GRID_GAP) {
 		ctx.moveTo(Y_AXIS_MARGIN, yPos(gridVal, maxValue, canvasHeight));
 		ctx.lineTo(canvasWidth, yPos(gridVal, maxValue, canvasHeight));
-		ctx.fillText(`${gridVal}%`, 0, yPos(gridVal, maxValue, canvasHeight));
+		ctx.fillText(`${gridVal}%`, legendXPos, yPos(gridVal, maxValue, canvasHeight));
 	}
 	ctx.stroke();
 }
