a51ee222fab5bdb69522bada4df14368d10a9173
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-15 18:21:48 +0900

    webapp - fix: AddStockTransaction 테스트 케이스의 'not wrapped in act...' 에러 해결

--- packages/webapp/src/pages/Holdings/__tests__/AddStockTransaction.test.tsx
+++ packages/webapp/src/pages/Holdings/__tests__/AddStockTransaction.test.tsx
@@ -2,57 +2,55 @@ import { render, screen } from '@testing-library/react';
 import { CustomWrapper } from '@lib/testingLibrary/react';
 import AddStockTransactionPage from '../ModalPage/AddNewStockTransaction';
 
-describe('Add stock transaction page layout', () => {
-	test('Should have a "거래내역 추가" header', () => {
-		render(
-			<CustomWrapper>
-				<AddStockTransactionPage portfolioId={-1} />
-			</CustomWrapper>
-		);
+test('Should have a "거래내역 추가" header', async () => {
+	render(
+		<CustomWrapper>
+			<AddStockTransactionPage portfolioId={-1} />
+		</CustomWrapper>
+	);
 
-		expect(screen.getByText(/거래내역 추가/)).toBeInTheDocument();
-	});
+	expect(await screen.findByText(/거래내역 추가/)).toBeInTheDocument();
+});
 
-	test('Should have a search stock input', () => {
-		render(
-			<CustomWrapper>
-				<AddStockTransactionPage portfolioId={-1} />
-			</CustomWrapper>
-		);
+test('Should have a search stock input', async () => {
+	render(
+		<CustomWrapper>
+			<AddStockTransactionPage portfolioId={-1} />
+		</CustomWrapper>
+	);
 
-		expect(screen.getByPlaceholderText('종목 검색...')).toBeInTheDocument();
-	});
+	expect(await screen.findByPlaceholderText('종목 검색...')).toBeInTheDocument();
+});
 
-	test('Should have select transaction type radio inputs', () => {
-		render(
-			<CustomWrapper>
-				<AddStockTransactionPage portfolioId={-1} />
-			</CustomWrapper>
-		);
+test('Should have select transaction type radio inputs', async () => {
+	render(
+		<CustomWrapper>
+			<AddStockTransactionPage portfolioId={-1} />
+		</CustomWrapper>
+	);
 
-		expect(screen.getByLabelText('매수')).toBeInTheDocument();
-		expect(screen.getByLabelText('매도')).toBeInTheDocument();
-	});
+	expect(await screen.findByLabelText('매수')).toBeInTheDocument();
+	expect(await screen.findByLabelText('매도')).toBeInTheDocument();
+});
 
-	test('Should have ticker, price, and quantity inputs', () => {
-		render(
-			<CustomWrapper>
-				<AddStockTransactionPage portfolioId={-1} />
-			</CustomWrapper>
-		);
+test('Should have ticker, price, and quantity inputs', async () => {
+	render(
+		<CustomWrapper>
+			<AddStockTransactionPage portfolioId={-1} />
+		</CustomWrapper>
+	);
 
-		expect(screen.getByLabelText('종목 티커')).toBeInTheDocument();
-		expect(screen.getByLabelText('가격')).toBeInTheDocument();
-		expect(screen.getByLabelText('수량')).toBeInTheDocument();
-	});
+	expect(await screen.findByLabelText('종목 티커')).toBeInTheDocument();
+	expect(await screen.findByLabelText('가격')).toBeInTheDocument();
+	expect(await screen.findByLabelText('수량')).toBeInTheDocument();
+});
 
-	test('Should have a submit button', () => {
-		render(
-			<CustomWrapper>
-				<AddStockTransactionPage portfolioId={-1} />
-			</CustomWrapper>
-		);
+test('Should have a submit button', async () => {
+	render(
+		<CustomWrapper>
+			<AddStockTransactionPage portfolioId={-1} />
+		</CustomWrapper>
+	);
 
-		expect(screen.getByRole('button', { name: '추가하기' })).toBeInTheDocument();
-	});
+	expect(await screen.findByRole('button', { name: '추가하기' })).toBeInTheDocument();
 });
