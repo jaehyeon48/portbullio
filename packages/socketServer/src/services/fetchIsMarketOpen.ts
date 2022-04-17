738598cbedafc4af1d9aea0fef3e09aa1756bd4e
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-17 09:16:50 +0900

    socket - feat: fetchIsMarketOpen 함수에 예외 처리 로직 추가

--- packages/socketServer/src/services/fetchIsMarketOpen.ts
+++ packages/socketServer/src/services/fetchIsMarketOpen.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { IsMarketOpen } from '@portbullio/shared/src/types';
+import logger from '@lib/winston';
 import envConfig from '@config';
 
 interface IsMarketOpenRes {
@@ -9,8 +10,14 @@ interface IsMarketOpenRes {
 }
 
 export default async function fetchIsMarketOpen(): Promise<IsMarketOpen> {
-	const { data }: IsMarketOpenRes = await axios.get(
-		`https://cloud.iexapis.com/stable/stock/TSLA/quote?filter=isUSMarketOpen&token=${envConfig.iexCloudApiKey}`
-	);
-	return data.isUSMarketOpen;
+	try {
+		const { data }: IsMarketOpenRes = await axios.get(
+			`https://cloud.iexapis.com/stable/stock/TSLA/quote?filter=isUSMarketOpen&token=${envConfig.iexCloudApiKey}`
+		);
+		return data.isUSMarketOpen;
+	} catch (error) {
+		const err = error as AxiosError;
+		logger.error(`fetchIsMarketOpen.ts: ${err.message}`);
+		return false;
+	}
 }
