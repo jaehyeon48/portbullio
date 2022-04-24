dbdcecad2485b93c8708dc38b2b9602948f88701
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-24 19:55:19 +0900

    server - fix: 워커 스레드 이슈 핫픽스
    워커 스레스가 ts-node 환경에서 제대로 동작하지 않는 이슈로 인해
    서버 실행 시 nodemon으로 실행하도록 변경

--- packages/server/src/services/stockTransaction/calculateAvgCost.ts
+++ packages/server/src/services/stockTransaction/calculateAvgCost.ts
@@ -1,10 +1,13 @@
+import { join } from 'path';
 import { Worker } from 'worker_threads';
 import { StockTransactionLog } from '@prisma/client';
 import { Holding } from '@portbullio/shared/src/types';
 
 export default function calculateAvgCost(transactions: StockTransactionLog[]) {
 	return new Promise<Holding[]>((resolve, reject) => {
-		const worker = new Worker('./src/worker/worker.js', { workerData: transactions });
+		const worker = new Worker(join(process.cwd(), 'src', 'worker', 'worker.ts'), {
+			workerData: transactions
+		});
 		worker.once('message', resolve);
 		worker.on('error', reject);
 		worker.on('exit', code => {
