4a3bbef7dccd7420b0070937e1e253810bcf8d95
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-25 15:12:18 +0900

    server - refactor: 사용하지 않는 'isUserExist' 함수 제거

--- packages/server/src/services/user/index.ts
+++ packages/server/src/services/user/index.ts
@@ -1,4 +1,3 @@
-export { default as isUserExist } from './isUserExist';
 export { default as getUserId } from './getUserId';
 export { default as createNewUser } from './createNewUser';
 export { default as getAvatar } from './getAvatar';
