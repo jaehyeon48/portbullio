5522e5521d9d1c4ec1845dc112fce6c0b5eaf074
jaehyeon48 <imjaehyeon48@gmail.com>
2022-04-26 20:53:28 +0900

    webapp - test: getSectors 테스트 케이스 수정
    query string을 반영하도록 mock 로직 수정

--- packages/webapp/src/lib/msw/mockData/stock/index.ts
+++ packages/webapp/src/lib/msw/mockData/stock/index.ts
@@ -1,5 +1,20 @@
 export const fakeTickers = ['AAPL', 'TSLA'];
-export const fakeGetSectorsResult = [
+
+export const fakeGetSectorsResultCase1 = [
+	{
+		ticker: 'AAPL',
+		sector: 'Technology'
+	}
+];
+
+export const fakeGetSectorsResultCase2 = [
+	{
+		ticker: 'TSLA',
+		sector: 'Consumer Cyclical'
+	}
+];
+
+export const fakeGetSectorsResultCase3 = [
 	{
 		ticker: 'AAPL',
 		sector: 'Technology'
