5494c89e32a8f61f972797661d2d2d585926fa73
jaehyeon48 <imjaehyeon48@gmail.com>
2022-05-01 11:06:25 +0900

    webapp - feat: 모바일 환경의 경우, 아직 최적화 되지 않았다는 alert 문구를 표시하는 로직 추가

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -4,6 +4,10 @@ import App from '@src/App';
 import { AppProviders } from '@components/index';
 import './font.css';
 
+if (window.matchMedia('(max-width: 1279px').matches) {
+	alert('현재 모바일 환경엔 최적화되어 있지 않습니다. 빠른 시일 내에 서비스하도록 하겠습니다 🙏');
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<AppProviders>
