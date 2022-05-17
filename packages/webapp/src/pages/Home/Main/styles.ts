543bd2b184680ef2ada95995737f92488cbeddcb
jaehyeon48 <imjaehyeon48@gmail.com>
2022-05-17 15:20:00 +0900

    webapp - feat: 홈 메인페이지 반응형 스타일 지원

--- packages/webapp/src/pages/Home/Main/styles.ts
+++ packages/webapp/src/pages/Home/Main/styles.ts
@@ -12,23 +12,56 @@ interface SectionProps extends Mixin.FlexProps {
 	margin?: string;
 }
 
-const midBotSectionLayout = css`
+const midBotSubSectionLayout = css`
+	height: 100%;
 	width: 30%;
-	max-width: 20em;
+	max-width: 310px;
 
-	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tabletLandScape}px) {
-		font-size: 16px;
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		height: 100%;
+		width: 70%;
+		max-width: 400px;
 	}
 `;
 
-export const Section = styled.section<SectionProps>`
-	${Mixin.flexCenter};
-	flex-direction: ${({ flexDirection }) => flexDirection};
-	align-items: ${({ alignItems }) => alignItems};
-	justify-content: ${({ justifyContent }) => justifyContent};
+const sectionStyle = css<SectionProps>`
+	display: flex;
+	justify-content: space-evenly;
 	max-width: 1600px;
-	margin: 0 auto;
-	margin: ${({ margin }) => margin};
+`;
+
+export const UpperSection = styled.section`
+	${sectionStyle};
+	padding: 14px 0 10px;
+
+	@media screen and (max-width: 1024px) {
+		align-items: center;
+		flex-direction: column;
+		gap: 16px;
+	}
+`;
+
+export const MidSection = styled.section`
+	${sectionStyle};
+	margin: 50px auto;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		flex-direction: column;
+		align-items: center;
+		gap: 18px;
+	}
+`;
+
+export const LowerSection = styled.section`
+	${sectionStyle};
+	margin: 0 auto 5em;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		flex-direction: column;
+		align-items: center;
+		gap: 30px;
+		margin: 0 auto;
+	}
 `;
 
 export const HeaderContainer = styled.div<HeaderContainerProps>`
@@ -40,10 +73,26 @@ export const Header = styled.header`
 	display: inline-block;
 	font-size: 48px;
 	font-weight: 700;
-	margin-bottom: 1.5em;
+	margin-bottom: 40px;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptop}px) {
+		font-size: 42px;
+	}
 
 	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tabletLandScape}px) {
-		font-size: 40px;
+		font-size: 36px;
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptopSmall}px) {
+		margin-bottom: 16px;
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		font-size: 30px;
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.mobileLandScape}px) {
+		font-size: 24px;
 	}
 `;
 
@@ -53,52 +102,81 @@ export const HeaderPrimary = styled.span`
 
 export const HeroImageContainer = styled.div`
 	display: inline-block;
-	margin: 1.5em 5em 1.5em 1.5em;
 
-	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptop}px) {
-		& > svg {
-			width: 500px;
-			height: 390px;
-		}
+	& > svg {
+		width: 460px;
+		height: 359px;
 	}
 
 	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tabletLandScape}px) {
 		& > svg {
-			width: 450px;
-			height: 351px;
+			width: 380px;
+			height: 296px;
+		}
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		& > svg {
+			width: 330px;
+			height: 257px;
+		}
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.mobileLandScape}px) {
+		& > svg {
+			width: 300px;
+			height: 234px;
+		}
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.mobile}px) {
+		& > svg {
+			width: 270px;
+			height: 211px;
 		}
 	}
 `;
 
-export const Button = styled.button`
+const homePageButtonStyle = css`
 	${Mixin.buttonMixin};
 	background-color: var(--primary);
 	color: var(--white);
 	font-weight: 500;
 	border-radius: 6px;
-	padding: 0.6em 3.2em;
+	padding: 8px 42px;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptop}px) {
+		padding: 8px 40px;
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		font-size: 14px;
+	}
+`;
+
+export const Button = styled.button`
+	${homePageButtonStyle};
 `;
 
 export const LinkButton = styled(Link)`
-	${Mixin.buttonMixin};
+	${homePageButtonStyle};
 	text-decoration: none;
-	background-color: var(--primary);
-	color: var(--white);
-	font-weight: 500;
-	border-radius: 6px;
-	padding: 0.6em 3.2em;
 `;
 
 export const IndexContainer = styled(Card)`
-	${midBotSectionLayout};
+	${midBotSubSectionLayout};
 `;
 
 export const IndexHeader = styled.header`
 	text-align: center;
 	font-size: 18px;
 	font-weight: 700;
-	padding: 0.4em 0;
+	padding: 6px 0;
 	border-bottom: 1px solid var(--indexCardHeaderBorderColor);
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tabletLandScape}px) {
+		font-size: 16px;
+	}
 `;
 
 export const IndexInfo = styled.div`
@@ -109,27 +187,49 @@ export const IndexPriceContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-evenly;
-	padding: 0.8em 0 0.5em;
+	padding: 10px 0 8px;
 `;
 
 export const IndexPrice = styled.span`
 	font-size: 22px;
 	font-weight: 700;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tabletLandScape}px) {
+		font-size: 20px;
+	}
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.mobile}px) {
+		font-size: 18px;
+	}
 `;
 
 export const IndexPriceChange = styled.span`
 	display: flex;
 	align-items: center;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptopSmall}px) {
+		font-size: 14px;
+
+		& svg {
+			width: 16px;
+			height: 16px;
+			margin-top: 2px;
+		}
+	}
 `;
 
 export const IndexChangePercent = styled.div`
-	padding: 0.3em 0 0.6em;
+	padding: 4px 0 6px;
 	font-size: 16px;
 	text-align: center;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.tablet}px) {
+		font-size: 14px;
+	}
 `;
 
 export const TopStocksListSection = styled.section`
-	${midBotSectionLayout};
+	${midBotSubSectionLayout};
 `;
 
 export const TopStocksListHeader = styled(Link)`
@@ -157,8 +257,12 @@ export const TopStocksListItems = styled.ul`
 
 export const TopStocksListItem = styled(Card)`
 	list-style-type: none;
-	margin-bottom: 1.2em;
+	margin-bottom: 16px;
 	cursor: pointer;
+
+	@media screen and (max-width: ${WIDTH_BREAK_POINT_PX.laptopSmall}px) {
+		font-size: 14px;
+	}
 `;
 
 export const TopStocksListItemLink = styled(Link)`
