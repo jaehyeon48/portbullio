e2f371d73e64c1030f1ebddb0e286568f18af0e7
jaehyeon48 <imjaehyeon48@gmail.com>
2022-05-19 09:02:20 +0900

    webapp - feat: 모바일 최적화작업 진행중임을 알리는 alert 메시지 삭제

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -4,10 +4,6 @@ import App from '@src/App';
 import { AppProviders } from '@components/index';
 import './font.css';
 
-if (window.matchMedia('(max-width: 810px').matches) {
-	alert('현재 모바일 환경엔 최적화되어 있지 않습니다. 빠른 시일 내에 서비스하도록 하겠습니다 🙏');
-}
-
 ReactDOM.render(
 	<React.StrictMode>
 		<AppProviders>
