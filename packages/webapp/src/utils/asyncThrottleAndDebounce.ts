e7051673240ad7dd289e2d74b645cc491afd4265
jhk48 <imjaehyeon48@gmail.com>
2022-06-01 22:53:53 +0900

    webapp - feat: 'asyncThrottleAndDebounce' 함수 구조 수정
    setTimeout을 기반으로 throttle 기능을 구현하던 방식에서, Date.now()를 사용한 방식으로 수정.
    이를 통해 자바스크립트 이벤트루프 특성상 마지막으로 처리된 시점으로부터 정확히 limit 이후에
    다음 함수를 처리할 수 없다는 한계를 극복할 수 있을 것으로 기대됨.

--- packages/webapp/src/utils/asyncThrottleAndDebounce.ts
+++ packages/webapp/src/utils/asyncThrottleAndDebounce.ts
@@ -6,17 +6,14 @@ export default function asyncThrottleAndDebounce<T extends (...args: any) => any
 	callback: T,
 	limit: number
 ): ThrottledFunction<T> {
-	let wait = false;
+	let lastCalledTime = 0;
 	let timer: NodeJS.Timeout;
 	return async (...args: any): Promise<ReturnType<any>> => {
 		clearTimeout(timer);
 		timer = setTimeout(() => callback(...args), limit);
-		if (!wait) {
-			callback(...args);
-			wait = true;
-			setTimeout(() => {
-				wait = false;
-			}, limit);
-		}
+		const now = Date.now();
+		if (now - lastCalledTime < limit) return;
+		callback(...args);
+		lastCalledTime = now;
 	};
 }
